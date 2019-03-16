733712cd38e2283b9b241a58151aa4f7eeb409c1
SteveMustanski <steve.mustanski@gmail.com>
2019-03-16 17:43:00 -0400

    added tests using jest, enzyme, shallow, and mount for comparison

--- src/components/courses/CourseForm.Snapshots.test.js
+++ src/components/courses/CourseForm.Snapshots.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import CourseForm from "./CourseForm";
 import renderer from "react-test-renderer";
 import { courses, authors } from "../../../tools/mockData";
-import { JestEnvironment } from "@jest/environment";
 
 it("sets the save button label to 'Saving' when saving is true.", () => {
   const tree = renderer.create(
---
+++ tools/testSetup.js
@@ -0,0 +1,4 @@
+// setup file to configrue adapter for enzyme
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
